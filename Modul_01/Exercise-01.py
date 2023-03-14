d1b894683eab7937f95f826d568d9a5b04d2e191
daffaulhaqqi <mdaffa32@gmail.com>
2023-03-14 10:10:06 +0700

    initial commit

---
+++ Modul_01/Exercise-01.py
@@ -0,0 +1,6 @@
+#Exercise 01 - I0322083 Muhammad Daffa'ul Haqqi Murti
+
+print("Muhammad Daffa'ul Haqqi Murti")
+print("I0322083")
+print("Tempat tinggalku beralamat di Jalan Veteran, Kecamatan Kartasura, Kabupaten Sukoharjo, lebih tepatnya di Perumahan Safira Residence")
+print("Harapan saya sebagai lulusan Teknik Industri nanti adalah semoga saya bisa mempergunakan ilmu yang dipelajari selama kuliah dengan baik dan dapat mengaplikasikannya pada lingkup pekerjaan saya.")
